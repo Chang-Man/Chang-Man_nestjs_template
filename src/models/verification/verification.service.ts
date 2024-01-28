923bd9c58f81c86426497fb3c36bbc41b8230eae
김창아 <changagim@snu.ac.kr>
2024-01-28 16:20:56 +0900

    uninstall aligoapi, install axios @nestjs/axios

--- src/models/verification/verification.service.ts
+++ src/models/verification/verification.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { VerificationRepository } from './verification.repository';
 
 @Injectable()
 export class VerificationService {
+  constructor(
+    private readonly verificationRepository: VerificationRepository,
+  ) {}
   async verify(phone: string) {
     console.log(phone);
   }
