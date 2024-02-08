bba63c1563f6f9d9a9f402fed419fde03b9a3171
김창아 <changagim@snu.ac.kr>
2024-02-08 17:49:42 +0900

    constant

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
-import { ValidationPipe } from '@nestjs/common';
 import { setNestApp } from './common/base/setNestApp';
 
 async function bootstrap() {
